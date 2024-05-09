00dbc0392ad664eacc2a21b4d2083ec45be916c5
Kamela <kamela_ae@proton.me>
2024-05-09 21:17:11 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kamela.vercel.app',
   output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true }
